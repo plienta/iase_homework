9dde2f4f41f9de6d48a68f81965849ed439570e7
Phương Liên Tạ <phuonglienta2121999@gmail.com>
2026-04-28 16:48:18 +0200

    add runnable showStandings

--- src/main/kotlin/de/seuhd/worldcup/Data.kt
+++ src/main/kotlin/de/seuhd/worldcup/Data.kt
@@ -14,13 +14,64 @@ data class Group(
     val name: String = "",
     var teams: List<Team>,
     var matches: List<Match>
-)
+) {
+    fun initializeTeamObjectsInMatches() {
+        for (match in matches) {
+            match.initializeTeams(teams)
+        }
+    }
+
+    fun sortTeams(): List<Team> {
+        val sorted = teams.sortedWith(
+            compareByDescending<Team> { it.teamStat?.totalPoints }
+                .thenByDescending { it.teamStat?.totalGoalsDiff }
+        )
+        return sorted
+    }
+
+    fun calculatePointsForAllTeamInThisGroup() {
+        val validMatches = matches.filter { match -> match.homeScore != null && match.awayScore != null }
+        for (match in validMatches) {
+            val homePointThisMatch = getPointForFirstTeam(match.homeScore!!, match.awayScore!!);
+            val awayPointThisMatch = getPointForFirstTeam(match.awayScore!!, match.homeScore!!);
+
+            match.homeTeamObj?.teamStat?.totalPoints += homePointThisMatch;
+            match.homeTeamObj?.teamStat?.totalGoalsFor += match.homeScore!!;
+            match.homeTeamObj?.teamStat?.totalGoalsAgainst += match.awayScore!!;
+            match.homeTeamObj?.teamStat?.totalGoalsDiff += (match.homeScore!! - match.awayScore!!);
+
+            match.awayTeamObj?.teamStat?.totalPoints += awayPointThisMatch;
+            match.awayTeamObj?.teamStat?.totalGoalsFor += match.awayScore!!;
+            match.awayTeamObj?.teamStat?.totalGoalsAgainst += match.homeScore!!;
+            match.awayTeamObj?.teamStat?.totalGoalsDiff += (match.awayScore!! - match.homeScore!!);
+        }
+    }
+
+    fun getPointForFirstTeam(firstScore: Int, secondScore: Int): Int {
+        if (firstScore < secondScore) {
+            return 0;
+        }
+        if (firstScore > secondScore) {
+            return 3;
+        }
+        return 1;
+    }
+}
 
 @Serializable
 data class Team(
     val id: String = "",
-    val name: String? = null
+    val name: String? = null,
+    var teamStat: TeamStat? = TeamStat()
 )
+
+@Serializable
+class TeamStat {
+    var totalPoints: Int = 0
+    var totalGoalsFor: Int = 0
+    var totalGoalsAgainst: Int = 0
+    var totalGoalsDiff: Int = 0
+}
 
 @Serializable
 data class Match(
@@ -32,7 +83,17 @@ data class Match(
     var homeScore: Int? = null,
     var awayScore: Int? = null,
     val ground: String? = null,
-)
+
+    // convert data from string to object
+    var homeTeamObj: Team? = null,
+    var awayTeamObj: Team? = null
+
+) {
+    fun initializeTeams(teams: List<Team>) {
+        homeTeamObj = teams.find { t -> t.id == homeTeam }
+        awayTeamObj = teams.find { t -> t.id == awayTeam }
+    }
+}
 
 @Serializable
 data class Knockout(
--- src/main/kotlin/de/seuhd/worldcup/Main.kt
+++ src/main/kotlin/de/seuhd/worldcup/Main.kt
@@ -1,23 +1,48 @@
 package de.seuhd.worldcup
 
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.decodeFromString
 
 fun main() {
+    val tournament = readData()
+    val testGroups = mutableListOf<Group>(tournament.groups[0]);
+    showStandings(testGroups);
+}
+
+private fun readData(): WorldCupData {
     val jsonString = object {}.javaClass.getResource("/world_cup_2026_full_data.json")!!
         .readText()
     val tournament = Json.decodeFromString<WorldCupData>(jsonString)
-    println(tournament.groups[0].matches[0].homeTeam)
-
-    //TODO: Implement interactive menu
+    return tournament
 }
 
 /* -------------------------------------------------------------
    1) Show Standings
    ------------------------------------------------------------- */
+// This run with the assumption that groups has been filtered
 private fun showStandings(allGroups: List<Group>) {
-    //TODO
+    for (group in allGroups) {
+        group.initializeTeamObjectsInMatches()
+        group.calculatePointsForAllTeamInThisGroup();
+        val sorted = group.sortTeams();
+        printStandingsTable(group.name, sorted);
+    }
+}
+
+private fun printStandingsTable(groupName: String, sorted: List<Team>) {
+    println("== $groupName ==")
+    println("Pos | Team            | Pts | GF | GA | GD")
+    println("-------------------------------------------")
+
+    sorted.forEachIndexed { index, t ->
+        println(
+            "${index + 1}   | " +
+                    "${t.name?.padEnd(15)} | " +
+                    "${t.teamStat?.totalPoints.toString().padEnd(3)} | " +
+                    "${t.teamStat?.totalGoalsFor.toString().padEnd(3)} | " +
+                    "${t.teamStat?.totalGoalsAgainst.toString().padEnd(3)} | " +
+                    "${t.teamStat?.totalGoalsDiff}"
+        )
+    }
 }
 
 /* -------------------------------------------------------------
