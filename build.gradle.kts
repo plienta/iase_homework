76526edb1530b9f57e107bfd730da770bc25f303
Phương Liên Tạ <phuonglienta2121999@gmail.com>
2026-04-28 14:42:14 +0200

    fix config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.3.20"
     kotlin("plugin.serialization") version "2.3.20"
+    id("application")
 }
 
 group = "org.example"
@@ -8,6 +9,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass.set("de.seuhd.worldcup.MainKt")
 }
 
 dependencies {
